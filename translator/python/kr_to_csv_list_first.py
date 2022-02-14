387055d6cb22db8c5ab30d753a9cd408f25afebe
Kiseop Lee <kiseop.lee@gmail.com>
2022-02-14 15:16:09 +0900

    csv 종류별로 분리한 것에 맞춰 수정
    csv 종류별로 분리한 것에 맞춰 수정

---
+++ translator/python/kr_to_csv_list_first.py
@@ -0,0 +1,60 @@
+#-*- coding: utf-8 -*-
+import csv
+import os
+
+path_dir = '../../PoeCharm/Pob/translate_cn'
+f_list = os.listdir(path_dir)
+
+result_dir = '../translate_kr'
+tr_kr_orig = '../../PoeCharm/Pob/translate_kr.csv'
+tr_kr_dir = '../../PoeCharm/Pob/translate_kr'
+tr_kr_etc = '../../PoeCharm/Pob/translate_kr/etcs.csv'
+
+tr_kr = {}
+etc_kr = {}
+tr_kr_key = {}
+with open(tr_kr_orig, 'r', encoding='utf8') as csvfile:
+  read_csv = csv.reader(csvfile)
+  for row in read_csv:
+    tr_kr[row[0].strip()] = row[1]
+    etc_kr[row[0].strip()] = row[1]
+    tr_kr_key[row[0].strip().upper()] = row[0]
+del tr_kr['k']
+
+for f in f_list:
+  new_kr = {}
+  need_kr = {}
+  tr_cn = {}
+  tr_cn_key = {}
+  with open((path_dir + '/' + f), 'r', encoding='utf8') as csvfile:
+    read_csv = csv.reader(csvfile)
+    for row in read_csv:
+      tr_cn[row[0].strip()] = row[1]
+      tr_cn_key[row[0].strip().upper()] = row[0]
+  
+  for key, val in tr_cn_key.items():
+    try:
+      if(tr_cn_key[key].strip().upper() == tr_kr_key[key].strip().upper()):
+        if(tr_cn_key[key].strip().upper() == 'GUARDIAN'):
+          print(tr_cn_key[key].strip().upper())
+        new_kr[tr_cn_key[key]] = tr_kr[tr_kr_key[key].strip()]
+        del etc_kr[tr_cn_key[key]]
+    except KeyError:
+      need_kr[tr_cn_key[key]] = ''
+      pass
+
+  with open(result_dir + '/' + f, 'w') as csvfile:
+      spamwriter = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_ALL, escapechar=None)
+      for key, val in new_kr.items():
+        spamwriter.writerow([key, val])
+  
+with open(result_dir + '/etcs.csv', 'w') as csvfile:
+    spamwriter = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_ALL, escapechar=None)
+    for key, val in etc_kr.items():
+      spamwriter.writerow([key, val])
+  
+with open('../need.csv', 'w') as csvfile:
+    spamwriter = csv.writer(csvfile, delimiter='	')
+    for key, val in need_kr.items():
+      spamwriter.writerow([key, val])
+
