788a4f66042c555f6866288ac5b8c35df8abe892
kiseop.lee <kiseop.lee@gmail.com>
2023-02-23 00:58:46 +0900

    window일 경우 newline 부분은 제거하도록 함
    window일 경우 newline 부분은 제거하도록 함

--- translator/python/db_to_files.py
+++ translator/python/db_to_files.py
@@ -17,8 +17,8 @@ if(os.path.isdir(result_dir) != True):
 ###########################################################
 # DB read
 
-# creating file path
-dbfile = '../../resources.db'
+# get db file path
+dbfile = '../resources.db'
 # Create a SQL connection to our SQLite database
 con = sqlite3.connect(dbfile)
 
@@ -31,34 +31,20 @@ table_name = result[0][0]
 
 result = [a for a in cur.execute("SELECT k, v, source FROM " + table_name)]
 db_data = {}
+tr_file_names = []
 
 for data in result:
-  if (data[2] == 'treenode_name'):
-    continue
-  if (data[2] == 'treenode_optinon'):
-    continue
-  if (data[2] == 'Items_data'):
-    continue
+  if data[2] not in tr_file_names:
+    tr_file_names.append(data[2])
   if data[2] not in db_data.keys():
     db_data[data[2]] = {}
   db_data[data[2]][data[0]] = data[1]
 
 ###########################################################
 # File Names Read
-tr_data = {}
-tr_file_names = {}
 
-for f in kr_list:
-  with open((kr_dir + '/' + f), 'r', encoding='utf8') as csvfile:
-    if(f == tr_kr_orig):
-      continue
-    if(f == mac_file):
-      continue
-    tr_data[f.split('.')[0]] = {}
-    tr_file_names[f.split('.')[0]] = f
-
-for key1 in db_data:
-  with open(result_dir + '/' + tr_file_names[key1], 'w', encoding='utf8') as csvfile:
+for key1 in tr_file_names:
+  with open(result_dir + '/' + key1 + '.csv', 'w', newline='', encoding='utf8') as csvfile:
       spamwriter = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_ALL, escapechar=None)
       for key, val in db_data[key1].items():
         spamwriter.writerow([key, val])
